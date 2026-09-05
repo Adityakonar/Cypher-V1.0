d9612d62c0d7717c32047b00fbd7b7e22e97e241
Aditya konar <konaraditya19@gmail.com>
2026-09-05 21:10:30 +0530

    New

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import speech_recognition as sr
 import win32com.client
 import webbrowser
+import datetime
+import os
 
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
@@ -19,7 +21,6 @@ def takeCommand():
         except Exception as e:
             return "Please Repeat Closer to the microphone"
 if __name__ == '__main__':
-    say("Hello sir, How may I help you?")
     print("Listening.....")
     query=takeCommand()
     sites= [["youtube","https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google","https://www.google.com"],["chat gpt","https://www.chatgpt.com"]]
@@ -27,4 +28,13 @@ if __name__ == '__main__':
         if f"open {site[0]}".lower() in query.lower():
             say(f"Opening{site[0]}")
             webbrowser.open(site[1])
+    if "the time" in query:
+        strfTime=datetime.datetime.now().strftime("%H:%M:%S")
+        print(f"The time is {strfTime}")
+        say(f"The time is {strfTime}")
+
+    if "open blender"in query:
+        os.startfile(r"C:\Program Files\Blender Foundation\Blender 5.1\blender.exe")
+    if "open autocad".lower() in query.lower():
+        os.startfile(r"C:\Program Files\Autodesk\AutoCAD 2027\acad.exe")
             
